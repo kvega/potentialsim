5c858411e5aa94b664b2df21cbe0f3526e67bf12
kvega <kevin.n.vega@gmail.com>
2017-05-06 23:14:40 -0700

    set TODOs, began implementation of Particle class

---
+++ src/potentialsim.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import pylab
+import random
+
+"""
+Generates a text file of values representing the coordinates of n particles in a
+potential.
+"""
+
+# TODO: Create the Particle Class
+class Particle(object):
+    """
+    Representation of a simple, non-interacting, massive particle
+    (assumes point-like, does not model charge).
+    """
+    def __init__(self, position=[], velocity=[], mass=1.0):
+        """
+        TODO: define attributes
+        """
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+
+    def get_position(self):
+        return self.position
+
+    def get_velocity(self):
+        return self.velocity
+
+
+
+# TODO: Implement the equations of motion for a given potential
+
+# TODO: Populate the space
